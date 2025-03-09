2c2293487dd85cea1e59aa0689ae04186ec51824
Mishal <mishalalexander20@gmail.com"n>
2025-03-10 01:42:53 +0530

    adding comments
    Signed-off-by: Mishal <mishalalexander20@gmail.com"n>

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+// dependencies - express (api server), dotenv(sensitive data encryption), cors(to resolve cors errors)
+// db - from the utils folder
 import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
 import db from "./utils/db.js";
 
+// mandatory function call to use dotenv file
 dotenv.config();
 
+// express is initialised
 const app = express();
+
+// setting up express to work with cors, json and ASCII values
 app.use(
     cors({
         origin: 'http://localhost:3000',
@@ -13,21 +19,28 @@ app.use(
         method: ['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization']
     }));
+
 app.use(express.json());
+
 app.use(express.urlencoded({ extented: true }));
 
+// getting the port from the dotenv file; if that fails, set it to 4000
 const port = process.env.PORT || 4000;
 
+// declaring a server route to '/' which returns 'Cohort!' to caller
 app.get('/', (req, res) => {
     res.send('Cohort!');
 })
 
+// declaring a server route to '/mishal' which returns 'Mishal!' to caller
 app.get('/mishal', (req, res) => {
     res.send('Mishal!')
 })
 
+// connecting to the db
 db();
 
+// server runs and is open to connections at the given port
 app.listen(port, () => {
     console.log(`Application running at port ${port}`);
 });
--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,20 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
+// re-importing dotenv package just in case
 dotenv.config();
 
+// using the monegoose.connect command, we are trying to connect to db by fetching the connection url from dotenv file
 const db = () => {
     mongoose
         .connect(process.env.MONGODB_URL)
         .then(() => {
-            console.log("Connected to mongodb")
+            console.log("Connected to mongodb successfully!")
         })
         .catch((err) => {
             console.log(`Error connecting to mongodb: ${err}`)
         });
 }
 
+// exporting this db function to be imported into other files
 export default db;
