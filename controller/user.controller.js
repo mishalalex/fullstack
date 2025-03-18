c76ec24408ddd20d5779e2d87487adf7dc8b3987
Mishal <mishalalexander20@gmail.com"n>
2025-03-18 21:02:10 +0530

    adding comments for better understanding
    Signed-off-by: Mishal <mishalalexander20@gmail.com"n>

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -7,45 +7,47 @@ dotenv.config();
 
 const registerUser = async (req, res) => {
 
-    // create a token
-    // save token in db
-    // send token to user via email
-    // return success message to user
-
-
+    // get the required data from user from the request 
     const { name, email, password } = req.body;
+    // verify whether the mandatory data is send by the user
     if (!name || !email || !password) {
         res.status(400).json({
             message: "All fields are required."
         })
     }
 
-    console.log(email);
-
     try {
+        // validate whether such a user exist in our database
         const existingUser = await User.findOne({ email });
+        // return the error message to user if the user already exists
         if (existingUser) {
             res.status(400).json({
                 message: "User already exists. Please login."
             })
         }
+        // once verified, create a new user in db
         const newUser = await User.create({
             name,
             email,
             password
         });
 
+        // if something goes wrong, return an error to the user
         if (!newUser) {
             res.status(500).json({
                 message: `User is not registered. Please contact admin.`
             });
         }
 
+        // generate a token which can be given to the user to verify the user's email
         const token = crypto.randomBytes(32).toString("hex");
+        // save this token in the database
         newUser.verificationToken = token;
 
+        // update the database with all the above mentioned changes
         await newUser.save();
 
+        // create the pre-requisites for sending email using nodemailer (will beautify this code later)
         const transporter = nodemailer.createTransport({
             host: process.env.MAILTRAP_HOST,
             port: process.env.MAILTRAP_PORT,
@@ -63,8 +65,10 @@ const registerUser = async (req, res) => {
             text: `Please click on this link: ${process.env.BASE_URL}/api/v1/users/verify/${token}`, // plain text body
         }
 
+        // send the email to the user with the verification code
         await transporter.sendMail(mailOptions);
 
+        // once done, we can return the response to client stating the user is successfully registered
         res.status(201).json({
             message: "User is successfully registered.",
             success: true
@@ -80,33 +84,38 @@ const registerUser = async (req, res) => {
 
 const verifyUser = async (req, res) => {
 
+    // get the verification token from the api request parameter
     const { token } = req.params;
+    // verify whether a token is sent by the user
     if (!token) {
         res.status(400).json({
             message: "Token is missing."
         })
     }
 
-    console.log(token);
-
     try {
+        // check whether the token sent by the user exists in our database
         const verifyThisUser = await User.findOne({
             verificationToken: token
         })
 
+        // return an error response to the user if the token sent by the user doesn't exist in our database
         if (!verifyThisUser) {
             res.status(400).json({
                 message: "Invalid token"
             })
         }
 
+        // set the user as verified and delete the verification token from the user's data
         verifyThisUser.isVerified = true;
         verifyThisUser.verificationToken = null;
 
+        // save the above changes to the user
         await verifyThisUser.save();
 
         console.log(verifyThisUser)
 
+        // once done, we can successfully tell the client that the user is verified
         res.status(200).json({
             message: "User has been successfully verified. Please login to continue"
         })
